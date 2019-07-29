db5668defd1e8869a2a0b60c3f70d7c970b15c0a
d.kozhushko <d.kozhushko@informika.ru>
2019-07-29 11:58:19 +0300

    build in docs folder for github pages

---
+++ docs/precache-manifest.5299765b622f7c36337fcc9ccb75dff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cec54862c7954133a43f04912f35ccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40c24ab8cc72a517df1c",
+    "url": "/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "33e8a1142c2574f43592",
+    "url": "/static/css/main.19e6aab2.chunk.css"
+  },
+  {
+    "revision": "40c24ab8cc72a517df1c",
+    "url": "/static/js/2.519c64f2.chunk.js"
+  },
+  {
+    "revision": "33e8a1142c2574f43592",
+    "url": "/static/js/main.231c2bad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
